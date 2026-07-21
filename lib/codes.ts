a1b9a9422ad0874e130c3535bd6ae78cb612db24
Zahar-Bernyk <98636876+Zahar-Bernyk@users.noreply.github.com>
2026-07-21 10:18:13 -0500

    Delete lib directory

--- lib/codes.ts
+++
@@ -1,9 +0,0 @@
-// Код бронювання у форматі 'PM-####'
-export function generateBookingCode(): string {
-  return 'PM-' + Math.floor(4830 + Math.random() * 9000);
-}
-
-// Код бронювання номера готелю у форматі 'HR-####' (Hotel Reservation).
-export function generateRoomCode(): string {
-  return 'HR-' + Math.floor(4830 + Math.random() * 9000);
-}
